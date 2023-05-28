6fa94e2d89685ed976ae4a7cd3fb89fc91d3098b
André Costa <andrefonsecacosta@gmail.com>
2023-05-27 20:09:28 -0700

    Refactor signal factory

--- src/signals/factory.ts
+++ src/signals/factory.ts
@@ -45,72 +45,112 @@ export type SignalFactory<TValue> = {
   ) => Signal<TContext, TValue>;
 };
 
+function createSignal<TContext, TValue>(
+  assert: (value: unknown) => TValue,
+  fn: (context: TContext) => TValue | Promise<TValue>,
+): Signal<TContext, TValue> {
+  return {
+    _assert: assert,
+    evaluate: async context => assert(fn(context)),
+  } as Signal<TContext, TValue>;
+}
+
+function addOperators<TContext, TValue>(
+  signal: Signal<TContext, TValue>,
+): Signal<TContext, TValue> {
+  return {
+    ...signal,
+    equals: value => new SignalRule(operator.$eq, signal, value),
+    in: values => new SignalRule(operator.$in, signal, values),
+  };
+}
+
+function addArrayOperators<TContext, TValue>(
+  signal: Signal<TContext, TValue>,
+): Signal<TContext, TValue> {
+  const arraySignal = signal as unknown as Signal<TContext, Array<unknown>>;
+  return {
+    ...signal,
+    contains: value => new SignalRule(operator.$all, arraySignal, [value]),
+    containsEvery: values => new SignalRule(operator.$all, arraySignal, values),
+    containsSome: values => new SignalRule(operator.$any, arraySignal, values),
+    every: rule => new SignalRule(operator.$and, arraySignal, [rule]),
+    some: rule => new SignalRule(operator.$or, arraySignal, [rule]),
+  };
+}
+
+function addBooleanOperators<TContext, TValue>(
+  signal: Signal<TContext, TValue>,
+): Signal<TContext, TValue> {
+  const booleanSignal = signal as unknown as Signal<TContext, boolean>;
+  return {
+    ...signal,
+    isFalse: () => new SignalRule(operator.$eq, booleanSignal, false),
+    isTrue: () => new SignalRule(operator.$eq, booleanSignal, true),
+  };
+}
+
+function addNumberOperators<TContext, TValue>(
+  signal: Signal<TContext, TValue>,
+): Signal<TContext, TValue> {
+  const numberSignal = signal as unknown as Signal<TContext, number>;
+  return {
+    ...signal,
+    greaterThan: value => new SignalRule(operator.$gt, numberSignal, value),
+    greaterThanOrEquals: value =>
+      new SignalRule(operator.$gte, numberSignal, value),
+    lowerThan: value => new SignalRule(operator.$lt, numberSignal, value),
+    lowerThanOrEquals: value =>
+      new SignalRule(operator.$lte, numberSignal, value),
+  };
+}
+
+function addStringOperators<TContext, TValue>(
+  signal: Signal<TContext, TValue>,
+): Signal<TContext, TValue> {
+  const stringSignal = signal as unknown as Signal<TContext, string>;
+  return {
+    ...signal,
+    endsWith: value => new SignalRule(operator.$sfx, stringSignal, value),
+    includes: value => new SignalRule(operator.$inc, stringSignal, value),
+    matches: value => new SignalRule(operator.$rx, stringSignal, value),
+    startsWith: value => new SignalRule(operator.$pfx, stringSignal, value),
+  };
+}
+
+function addModifiers<TContext, TValue>(
+  signal: Signal<TContext, TValue>,
+): Signal<TContext, TValue> {
+  return {
+    ...signal,
+    not: new Proxy(signal, {
+      get: (target, property, receiver) => {
+        const value = Reflect.get(target, property, receiver);
+        return typeof value === 'function'
+          ? (...args: Array<unknown>) =>
+              new InverseRule(value.bind(target)(...args))
+          : value;
+      },
+    }),
+  };
+}
+
 export function type<TValue>(
   assert: (value: unknown) => TValue,
 ): SignalFactory<TValue> {
   return {
     value<TContext>(fn: (context: TContext) => TValue | Promise<TValue>) {
-      let signal = {
-        _assert: assert,
-        evaluate: async context => assert(fn(context)),
-      } as Signal<TContext, TValue>;
-      signal = {
-        ...signal,
-        equals: value => new SignalRule(operator.$eq, signal, value),
-        in: values => new SignalRule(operator.$in, signal, values),
-      };
-
-      const arraySignal = signal as unknown as Signal<TContext, Array<unknown>>;
-      signal = {
-        ...signal,
-        contains: value => new SignalRule(operator.$all, arraySignal, [value]),
-        containsEvery: values =>
-          new SignalRule(operator.$all, arraySignal, values),
-        containsSome: values =>
-          new SignalRule(operator.$any, arraySignal, values),
-        every: rule => new SignalRule(operator.$and, arraySignal, [rule]),
-        some: rule => new SignalRule(operator.$or, arraySignal, [rule]),
-      };
-
-      const booleanSignal = signal as unknown as Signal<TContext, boolean>;
-      signal = {
-        ...signal,
-        isFalse: () => new SignalRule(operator.$eq, booleanSignal, false),
-        isTrue: () => new SignalRule(operator.$eq, booleanSignal, true),
-      };
-
-      const numberSignal = signal as unknown as Signal<TContext, number>;
-      signal = {
-        ...signal,
-        greaterThan: value => new SignalRule(operator.$gt, numberSignal, value),
-        greaterThanOrEquals: value =>
-          new SignalRule(operator.$gte, numberSignal, value),
-        lowerThan: value => new SignalRule(operator.$lt, numberSignal, value),
-        lowerThanOrEquals: value =>
-          new SignalRule(operator.$lte, numberSignal, value),
-      };
-
-      const stringSignal = signal as unknown as Signal<TContext, string>;
-      signal = {
-        ...signal,
-        endsWith: value => new SignalRule(operator.$sfx, stringSignal, value),
-        includes: value => new SignalRule(operator.$inc, stringSignal, value),
-        matches: value => new SignalRule(operator.$rx, stringSignal, value),
-        startsWith: value => new SignalRule(operator.$pfx, stringSignal, value),
-      };
-
-      return {
-        ...signal,
-        not: new Proxy(signal, {
-          get: (target, property, receiver) => {
-            const value = Reflect.get(target, property, receiver);
-            return typeof value === 'function'
-              ? (...args: Array<unknown>) =>
-                  new InverseRule(value.bind(target)(...args))
-              : value;
-          },
-        }),
-      };
+      return [
+        addOperators,
+        addArrayOperators,
+        addBooleanOperators,
+        addNumberOperators,
+        addStringOperators,
+        addModifiers,
+      ].reduce(
+        (value, operation) => operation(value),
+        createSignal(assert, fn),
+      );
     },
   };
 }
