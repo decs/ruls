44bacdb62ffebaca6ea5bcad9b3c9abaf3fc556c
Marcelo T Prado <lelo@hey.com>
2023-07-01 12:36:12 -0300

    Export Rule type to consumers

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {rule} from './rules';
 export {signal} from './signals';
+
+export type {Rule} from './rules'
--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,4 +1,4 @@
-import type Rule from './rule';
+import type RuleType from './rule';
 
 import {operator} from '../core/operators';
 import GroupRule from './group';
@@ -6,14 +6,24 @@ import InverseRule from './inverse';
 import parse from './parse';
 
 export const rule = {
-  every<TContext>(rules: Array<Rule<TContext>>): Rule<TContext> {
+  every<TContext>(rules: Array<RuleType<TContext>>): RuleType<TContext> {
     return new GroupRule(operator.$and, rules);
   },
-  none<TContext>(rules: Array<Rule<TContext>>): Rule<TContext> {
+  none<TContext>(rules: Array<RuleType<TContext>>): RuleType<TContext> {
     return new InverseRule(new GroupRule(operator.$or, rules));
   },
   parse,
-  some<TContext>(rules: Array<Rule<TContext>>): Rule<TContext> {
+  some<TContext>(rules: Array<RuleType<TContext>>): RuleType<TContext> {
     return new GroupRule(operator.$or, rules);
   },
 };
+
+/**
+ * Allows you to define complex conditions and criteria for decision-making. It
+ * consists of one or more signals, which can be combined using logical
+ * operators to create intricate structures.
+ * 
+ * Takes a TContext argument which encapsulates the necessary information
+ * required by signals to make decisions and determine the outcome of rules.
+ */
+export type Rule <TContext> = RuleType<TContext>;
